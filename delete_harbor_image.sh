ef32aefd4520b46bd7e141c2b42580f8de0e951b
xiyangxixi <34201743+xiyangxixi@users.noreply.github.com>
2018-10-30 11:08:16 +0800

    通过API批量删除Harbor镜像

---
+++ delete_harbor_image.sh
@@ -0,0 +1,65 @@
+
+#! /bin/bash
+# 通过Harbor提供的API来批量删除镜像，人工删除费时费力
+# 经过测试发现，通过接口去删除时提供的是的标签，但实际上删除的时候通过的是镜像的IMAGE_ID,也就是说
+# 如果我把同一个镜像tag多次上传到harbor，通过借口删除时，只需要提供其中一个标签，那么和这个镜像的IMAGE_ID相同的镜像都会删除
+
+
+#### 项目个数
+lines=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/projects?" |grep "\"name\""|awk -F "\"" '{print $4}'|wc -l`
+##### 展示当前有几个项目
+echo "当前Harbor有一下几个项目:"
+for i in $(seq 1 $lines)
+do
+   ###########具体是啥项目
+   a=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/projects?" |grep "\"name\""|awk -F "\"" '{print $4}'|awk -v b=$i 'NR==b{print $1}'`
+   echo $i、$a
+done
+#######选择具体的项目
+read -p  "请输入序号(1~$lines):,查看其下的镜像仓库:" number
+if [ $number -ge 1 -a $number -le $lines ]
+then
+ #########选择的是哪个项目
+ c=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/projects?" |grep "\"name\""|awk -F "\"" '{print $4}'|awk -v b=$number 'NR==b{print $1}'`
+
+  d=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/projects?" |grep "$c" -C 2 |grep "project_id" |awk '{print $2}' |awk -F "," '{print $1}'`
+  #echo "\$d-----------$d"
+  e=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories?project_id=$d" | grep "\"name\"" |awk -F "\"" '{print $4}' |awk -F "/" '{print $2}'|wc -l`
+ #######简单展示
+  echo "项目$c下有以下镜像仓库："
+  for line in $(seq 1 $e)
+  do
+   #####具体的仓库名
+    f=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories?project_id=$d" | grep "\"name\"" |awk -F "\"" '{print $4}' |awk -F "/" '{print $2}'|awk -v g=$line 'NR==g{print $1}'`
+    echo $line、$f
+  done
+   read -p  "请输入序号(1~$e):,查看其下的镜像格式以及对应的数量:" num
+   if [ $num -ge 1 -a $num -le $e ]
+   then
+  #### 镜像仓库名字
+     h=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories?project_id=$d" | grep "\"name\"" |awk -F "\"" '{print $4}' |awk -F "/" '{print $2}'|awk -v g=$num 'NR==g{print $1}'`
+     echo "您选择的仓库是$h"
+  #### 标签类型种类个数
+#     i=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories/$c%2F$h/tags/" |grep "\"name\"" |awk -F"\"" '{print $4}' | cut -c -6 |sort -n |uniq|wc -l`
+     #echo $i
+ #### 标签类型以及个数
+    echo "镜像格式为：如果是10月，则为201810*"
+    curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories/$c%2F$h/tags/" |grep "\"name\"" |awk -F"\"" '{print $4}' | cut -c -6 |awk '{count[$1]++}END{for (i in count)print i,count[i]}'
+ ######输入镜像格式，进行删除
+     echo "如果想删除某种形式的镜像,请输入类型："
+     read image_format
+     ##########输入类型的所有镜像
+     images=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories/$c%2F$h/tags/" |grep "\"name\"" |awk -F"\"" '{print $4}'|grep $image_format|awk '{print $1}'`
+     #########统计镜像个数
+    count_image=`curl -s -u "admin:Harbor12345" -X GET -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories/$c%2F$h/tags/" |grep "\"name\"" |awk -F"\"" '{print $4}'|grep $image_format|wc -l`
+    for image_label in $images
+    do
+      curl -u "admin:Harbor12345" -X DELETE -H "Content-Type: application/json" "https://zhouhua.zaizai.com/api/repositories/$c%2F$h/tags/$image_label"
+    done
+      if [ $? -eq 0 ]
+      then
+        echo "删除成功"
+        echo "本次共删除$count_image个镜像"
+      fi
+ fi
+fi
